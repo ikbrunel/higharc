83398c01c7a9db5b554fed1a1790e52a4fff4588
Benjamin Vulpes <benv@ordering.app>
2020-08-31 01:32:35 -0500

    test creating a new smoothie via api

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,6 +1,6 @@
 from api.models import Smoothie
 from django.urls import reverse
-from json import loads
+from json import dumps, loads
 from rest_framework.test import APITestCase
 from uuid import uuid4
 
@@ -29,3 +29,17 @@ class ViewTests(APITestCase):
         self.assertEqual(1, len(response))
         self.assertEqual(str(smoothie.id), found_smoothie['id'])
         self.assertEqual(smoothie.name, found_smoothie['name'])
+
+    def test_create_a_new_smoothie(self):
+        url = reverse('smoothie-list')
+        smoothie_name = uuid4()
+
+        response = loads(self.client.post(
+            url,
+            data={'name': smoothie_name},
+            HTTP_ACCEPT='application/json').content)
+
+        found_smoothie = Smoothie.objects.get(id=response['id'])
+        self.assertEqual(
+            str(smoothie_name),
+            found_smoothie.name)
